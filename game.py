259f4389616a5a765073f05060faedd889c98a63
Jakub Szymanek <jkbszmnk@gmail.com>
2022-06-22 16:00:21 +0200

    Nowa funkcja

--- game.py
+++ game.py
@@ -7,9 +7,10 @@ class Gra:
     def __init__(self):
         self.rewards = [" ", "X"]
         self.character = "O"
+        self.speed = 0.1
         x = 100  # szerokosc  
         y = 20  # wysokosc
-        self.a = {i:[" " for z in range(1, x + 1)] for i in range(1, y + 1)}
+        self.a = {i:[" " for _ in range(1, x + 1)] for i in range(1, y + 1)}
     
     def menu(self):
         print(f""" 
@@ -18,8 +19,8 @@ class Gra:
         |2. Wybierz postac                               |
         |3. Samouczek                                    |  
         |4. Ekwipunek                                    |
-        |                                                |
-        |                                                |
+        |5. Test komend                                  |
+        |6. Wybiersz predkosc poruszania sie             |
         |                                                |
         |                                                |
         |                                                |
@@ -30,7 +31,7 @@ class Gra:
             self.start()
         elif user_choice == 2:
             os.system('cls')
-            self.character = str(input("Postać Domyslna 'O'. \nWybierz postac: "))
+            self.character = str(input(f"Aktualnie wybrana postac: '{self.character}'. \nWybierz postac: "))
             os.system('cls')
             self.menu()
         elif user_choice == 3:
@@ -41,13 +42,18 @@ class Gra:
             os.system('cls')
             print("Ekwipunek")
             self.menu()
+        elif user_choice == 5:
+            eval(input("Wpisz komende: "))
+            self.menu()
+        elif user_choice == 6:
+            self.speed = float(input("Wpisz predkosc (Np. 0.02): "))
+            self.menu()
     def game_board(self):
         os.system('cls')
-        for i in self.a:
-            if i == 1 or i == 20:
-                print(f"O{'=' * (len(self.a[1]) + 2)}O")
-            else:
-                print(f"| {''.join(self.a[i])} |")
+        print(f"O{'=' * (len(self.a[1]) + 2)}O")
+        for i in self.a:    
+            print(f"| {''.join(self.a[i])} |")
+        print(f"O{'=' * (len(self.a[1]) + 2)}O")
 
     def start(self):
         position_x = 10
@@ -57,29 +63,29 @@ class Gra:
         self.game_board()
 
         while True:
-            if keyboard.is_pressed("w"):
-                time.sleep(0.05)
+            if keyboard.is_pressed("w") and position_x != 1:
+                time.sleep(self.speed)
                 self.a[position_x][position_y] = " "
                 position_x -= 1
                 self.a[position_x][position_y] = self.character
                 self.game_board()
 
-            elif keyboard.is_pressed("s"):
-                time.sleep(0.05)
+            elif keyboard.is_pressed("s") and position_x != 20:
+                time.sleep(self.speed)
                 self.a[position_x][position_y] = " "
                 position_x += 1
                 self.a[position_x][position_y] = self.character
                 self.game_board()
 
-            elif keyboard.is_pressed("a"):
-                time.sleep(0.005)
+            elif keyboard.is_pressed("a") and position_y != 0:
+                time.sleep(self.speed / 2)
                 self.a[position_x][position_y] = " "
                 position_y -= 1
                 self.a[position_x][position_y] = self.character
                 self.game_board()
 
-            elif keyboard.is_pressed("d"):
-                time.sleep(0.005)
+            elif keyboard.is_pressed("d") and position_y != 99:
+                time.sleep((self.speed / 2))
                 self.a[position_x][position_y] = " "
                 position_y += 1
                 self.a[position_x][position_y] = self.character
