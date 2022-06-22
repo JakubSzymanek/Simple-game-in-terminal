24554f9436d2ea18cb169c444c80318cb15f921f
Jakub Szymanek <jkbszmnk@gmail.com>
2022-06-22 21:30:11 +0200

    Gra z Emoji

--- game.py
+++ game.py
@@ -5,11 +5,13 @@ import os
 
 class Gra:
     def __init__(self):
-        self.rewards = [" ", "X"]
-        self.character = "O"
+        self.monsters = ["👾", "👻", "🤖", "🐙", "🐉"]
+        self.character = "🔪"
         self.speed = 0.1
-        x , y = 100, 20
-        self.board = {i:[" " for _ in range(1, x + 1)] for i in range(1, y + 1)}
+        self.x , self.y = 20, 10
+        self.board = {i:["  " for _ in range(1, self.x + 1)] for i in range(1, self.y + 1)}
+        for i in range(2, self.x - 1):
+            self.board[random.randint(1, self.y)][i] = random.choice(self.monsters)
     
     def menu(self):
         print(f""" 
@@ -51,15 +53,15 @@ class Gra:
             self.menu()
 
         elif user_choice == 6:
-            self.speed = float(input("Wpisz predkosc (Np. 0.02): "))
+            self.speed = float(input("Wpisz predkosc (Np. 0.02s): "))
             self.menu()
 
     def game_board(self):
         os.system('cls')
-        print(f"O{'=' * (len(self.board[1]) + 2)}O")
+        print(f"O{'=' * (2 *(len(self.board[1]) + 1))}O")
         for i in self.board:    
             print(f"| {''.join(self.board[i])} |")
-        print(f"O{'=' * (len(self.board[1]) + 2)}O")
+        print(f"O{'=' * (2 *(len(self.board[1]) + 1))}O")
 
     def start(self):
         position_x = 10
@@ -72,43 +74,37 @@ class Gra:
 
             if keyboard.is_pressed("w") and position_x != 1:
                 time.sleep(self.speed)
-                self.board[position_x][position_y] = " "
+                self.board[position_x][position_y] = "  "
                 position_x -= 1
                 self.board[position_x][position_y] = self.character
                 self.game_board()
 
-            elif keyboard.is_pressed("s") and position_x != 20:
+            elif keyboard.is_pressed("s") and position_x != self.y:
                 time.sleep(self.speed)
-                self.board[position_x][position_y] = " "
+                self.board[position_x][position_y] = "  "
                 position_x += 1
                 self.board[position_x][position_y] = self.character
                 self.game_board()
 
             elif keyboard.is_pressed("a") and position_y != 0:
                 time.sleep(self.speed / 2)
-                self.board[position_x][position_y] = " "
+                self.board[position_x][position_y] = "  "
                 position_y -= 1
                 self.board[position_x][position_y] = self.character
                 self.game_board()
 
-            elif keyboard.is_pressed("d") and position_y != 99:
+            elif keyboard.is_pressed("d") and position_y != self.x - 1:
                 time.sleep((self.speed / 2))
-                self.board[position_x][position_y] = " "
+                self.board[position_x][position_y] = "  "
                 position_y += 1
                 self.board[position_x][position_y] = self.character
                 self.game_board()
 
             elif keyboard.is_pressed("esc"):
-                self.board[position_x][position_y] = " "
+                self.board[position_x][position_y] = "  "
                 os.system('cls')
                 self.menu()
                 
-        
-
-
-
-
-
 
 game = Gra()
 
