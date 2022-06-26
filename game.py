2410483fc32a5db0512dbf9b9ca3190588ff3384
Jakub Szymanek <jkbszmnk@gmail.com>
2022-06-26 11:45:26 +0200

    Update

--- game.py
+++ game.py
@@ -5,19 +5,20 @@ import os
 
 class Gra:
     def __init__(self):
-        self.monsters = ["👾", "👻", "🤖", "🐙", "🐉"]
+        self.obstacles = ["🌲", "🌳", "🌵"]
+        self.monsters = ["👾", "👻","🐙", "🐉"]
         self.character = "🔪"
         self.speed = 0.2
         self.x , self.y = 20, 10
         self.score_of_game = 0
-        self.position_x = 1
-        self.position_y = 0
         self.create_board()
 
     def create_board(self):
         self.board = {i:["  " for _ in range(1, self.x + 1)] for i in range(1, self.y + 1)}
         for i in range(2, self.x - 1):
+            self.board[random.randint(1, self.y)][i] = random.choice(self.obstacles)
             self.board[random.randint(1, self.y)][i] = random.choice(self.monsters)
+        self.menu()
     
     def menu(self):
         print(f""" 
@@ -73,7 +74,9 @@ class Gra:
         if self.score_of_game == self.x - 3:
             os.system('cls')
             print("Wygrales!")
+            self.score_of_game = 0
             time.sleep(2)
+            self.create_board()
             self.menu()
             
 
@@ -85,40 +88,42 @@ class Gra:
             print(f"| {''.join(self.board[i])} |")
         print(f"O{'=' * (2 *(len(self.board[1]) + 1))}O")
 
-    def move(self, position, char):
+    def move(self):
         time.sleep(self.speed)
-        self.board[self.position_x][self.position_y] = "  "
-        if char == "-" and position == "x":
-            self.position_x -= 1
-        elif char == "+" and position == "x":
-            self.position_x += 1
-        elif char == "-" and position == "y":
-            self.position_y -= 1
-        else:
-            self.position_y += 1
+        if self.board[self.position_x][self.position_y] in self.obstacles:
+            print("Przegrales!")
         self.score()   
         self.board[self.position_x][self.position_y] = self.character
         self.game_board()
 
 
     def start(self):
-        self.game_board()
+        self.position_x = 1
+        self.position_y = 0
         self.board[self.position_x][self.position_y] = self.character
         self.game_board()
 
         while True:
 
-            if keyboard.is_pressed("w") and self.position_x != 1:
-                self.move("x", "-")
+            if keyboard.is_pressed("w") and self.position_x != 1 and self.board[self.position_x - 1][self.position_y] not in self.obstacles:
+                self.board[self.position_x][self.position_y] = "  "
+                self.position_x -= 1
+                self.move()
 
-            elif keyboard.is_pressed("s") and self.position_x != self.y:
-                self.move("x", "+")
+            elif keyboard.is_pressed("s") and self.position_x != self.y and self.board[self.position_x + 1][self.position_y] not in self.obstacles:
+                self.board[self.position_x][self.position_y] = "  "
+                self.position_x += 1
+                self.move()
 
-            elif keyboard.is_pressed("a") and self.position_y != 0:
-                self.move("y", "-")
+            elif keyboard.is_pressed("a") and self.position_y != 0 and self.board[self.position_x][self.position_y - 1] not in self.obstacles:
+                self.board[self.position_x][self.position_y] = "  "
+                self.position_y -= 1
+                self.move()
 
-            elif keyboard.is_pressed("d") and self.position_y != self.x - 1:
-                self.move("y", "+")
+            elif keyboard.is_pressed("d") and self.position_y != self.x - 1 and self.board[self.position_x][self.position_y + 1] not in self.obstacles:
+                self.board[self.position_x][self.position_y] = "  "
+                self.position_y += 1
+                self.move()
 
             elif keyboard.is_pressed("esc"):
                 self.board[self.position_x][self.position_y] = "  "
